6a84c2990a075b9276efb196f2400e4f232c0b05
Fery <veryresto@gmail.com>
2018-06-08 19:23:12 +0900

    refine footer for new tab

--- soal/resources/themes/bootstrap/default_footer.php
+++ soal/resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,5 @@
 <hr>
 
 <div class="footer">
-    Powered by <a href="http://www.directorylister.com">Directory Lister</a> and <a href="https://www.heroku.com/">Heroku</a>
+    Powered by <a href="http://www.directorylister.com" target="_blank">Directory Lister</a> and <a href="www.heroku.com/" target="_blank">Heroku</a>
 </div>
