0729d4eaf75db4bdd7c69e99ac250bcbbb199f7f
Fery <veryresto@gmail.com>
2018-06-09 02:27:19 +0900

    localize bootstrap.min.css. change body font-size 14->16px

--- soal/resources/themes/bootstrap/index.php
+++ soal/resources/themes/bootstrap/index.php
@@ -18,7 +18,7 @@
         <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
 
         <!-- STYLES -->
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/bootstrap.min.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css">
 
