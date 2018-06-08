6d3a59601802a197bce0f9e9f43b3e4b67fbcb8c
Fery <veryresto@gmail.com>
2018-06-08 22:51:48 +0900

    add google tag UA-117273037-2

--- soal/resources/themes/bootstrap/index.php
+++ soal/resources/themes/bootstrap/index.php
@@ -3,6 +3,16 @@
 <html>
 
     <head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117273037-2"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-117273037-2');
+		</script>
+
 
         <title>Directory listing of <?php echo $lister->getListedPath(); ?></title>
         <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
@@ -16,6 +26,8 @@
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/directorylister.js"></script>
+		
+		
 
         <!-- FONTS -->
         <link rel="stylesheet" type="text/css"  href="//fonts.googleapis.com/css?family=Cutive+Mono">
