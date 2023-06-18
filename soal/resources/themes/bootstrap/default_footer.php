402310f832d5c62bb599de044df8f5d9229159cf
veryresto <veryresto@gmail.com>
2023-06-18 11:29:09 +0700

    change platform

--- soal/resources/themes/bootstrap/default_footer.php
+++ soal/resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,5 @@
 <hr>
 
 <div class="footer">
-    Powered by <a href="http://www.directorylister.com" target="_blank">Directory Lister</a> and <a href="http://www.heroku.com" target="_blank">Heroku</a>
+    Powered by <a href="http://www.directorylister.com" target="_blank">Directory Lister</a> and <a href="https://www.digitalocean.com/products/app-platform" target="_blank">DigitalOcean</a>
 </div>
